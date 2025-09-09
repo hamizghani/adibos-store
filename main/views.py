1dfb0c0f4bef840a46122af5921e58b566a25162
hamizghani <hamizghaniiii@gmail.com>
2025-09-09 17:07:02 +0700

    changes

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'app_name' : 'Adibos Store',
+        'name': 'Muhammad Hmaiz Ghani Ayusha',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
